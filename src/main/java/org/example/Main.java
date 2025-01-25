46c1e92e655e45ebd5b27926e588782f96843875
yuik23 <yui_karamatsu@yahoo.co.jp>
2025-01-25 14:38:54 +0900

    Add feature of mobile number checker

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,24 @@
 package org.example;
 
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class Main {
 
   public static void main(String[] args) {
-    System.out.println("Hello, World!");
+
+    Scanner scanner = new Scanner(System.in);
+
+    //国内のハイフンあり携帯電話番号のパターンを登録
+    Pattern operatorPattern = Pattern.compile(
+        "^090-[0-9]{4}-[0-9]{4}$|^080-[0-9]{4}-[0-9]{4}$|^070-[0-9]{4}-[0-9]{4}$|^050-[0-9]{4}-[0-9]{4}$");
+
+    System.out.print("携帯電話番号を入力してください: ");
+    //入力が指定した形式かどうかチェック
+    if (scanner.hasNext(operatorPattern)) {
+      System.out.println(scanner.next() + " は有効な携帯電話番号です。");
+    } else {
+      System.out.println(scanner.next() + " は無効な携帯電話番号です。");
+    }
   }
 }
